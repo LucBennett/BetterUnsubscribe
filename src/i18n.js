0db9baff8f25fe1ef9a640896bbcd041efd042c2
Luc Bennett <61855787+LucBennett@users.noreply.github.com>
2026-08-08 21:30:34 -0700

    fix: replace `browser.i18n` with `messenger.i18n` in i18n.js for consistency

--- src/i18n.js
+++ src/i18n.js
@@ -6,6 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
   let elements = document.querySelectorAll('[data-i18n]');
   for (let element of elements) {
     let messageKey = element.getAttribute('data-i18n');
-    element.textContent = browser.i18n.getMessage(messageKey);
+    element.textContent = messenger.i18n.getMessage(messageKey);
   }
 });
